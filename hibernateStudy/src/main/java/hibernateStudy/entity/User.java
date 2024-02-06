69111299e3fa100c1df77077a31005cb9f60122d
Piyush Lovanshi <piyush.lovanshi@gmail.com>
2024-02-06 13:55:25 +0530

    FEAT :: @ManyToMany bi-directional

--- hibernateStudy/src/main/java/hibernateStudy/entity/User.java
+++ hibernateStudy/src/main/java/hibernateStudy/entity/User.java
@@ -1,10 +1,13 @@
 package hibernateStudy.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -20,6 +23,13 @@ public class User {
 	
 	private String name;
 
+	/*
+	 * mappedby value is the name of the variable representing the Set<User> in the
+	 * Group class
+	 */
+	@ManyToMany(mappedBy = "users")
+	private List<Group> groups;
+	
 	public User(String name) {
 		super();
 		this.name = name;
